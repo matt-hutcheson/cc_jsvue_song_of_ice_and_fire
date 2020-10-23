ad9261a63dfc7dc1f0ec1f81fca12161877bf2fd
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2020-10-23 16:59:08 +0100

    added unique ID to fetch results

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template lang='html'>
   <main>
     <ul>
-      <li v-for="(character,index) in gotCharacters" :key="index">
-        {{character.name}}
+      <li v-for="(character,index) in gotCharacters" v-if="character.name" :key="index">
+        {{character.name}} {{character.id}}
       </li>
     </ul>
     <ul>
       <li v-for="(house,index) in gotHouses" :key="index">
-        {{house.name}}
+        {{house.name}} {{house.id}}
       </li>
     </ul>
   </main>
@@ -34,7 +34,7 @@ export default {
       for (const page of pages) {
         fetch(`https://www.anapioficeandfire.com/api/characters?page=${page}&pageSize=50`)
         .then(result => result.json())
-        .then(jresult => {for (const jcharacter of jresult) {if (jcharacter.name) {this.gotCharacters.push(jcharacter)}}})
+        .then(jresult => {for (const jcharacter of jresult) {jcharacter.id = this.getEndOfUrl(jcharacter.url); this.gotCharacters.push(jcharacter)}})
         .then(() => this.gotCharacters.sort(this.sortAlphabetical));
       }
     },
@@ -44,25 +44,35 @@ export default {
       for (const page of pages) {
         fetch(`https://www.anapioficeandfire.com/api/houses?page=${page}&pageSize=50`)
         .then(result => result.json())
-        .then(jresult => {for (const jhouse of jresult) {this.gotHouses.push(jhouse)}})
+        .then(jresult => {for (const jhouse of jresult) {jhouse.id = this.getEndOfUrl(jhouse.url); this.gotHouses.push(jhouse)}})
         .then(() => this.gotHouses.sort(this.sortAlphabetical))
       }
     },
     sortAlphabetical: function(a,b) {
-          const nameA = a.name.toUpperCase();
-          const nameB = b.name.toUpperCase();
-          if (nameA < nameB) {
-            return -1;
-          }
-          if (nameA > nameB) {
-            return 1;
-          }
-          return 0;
-        }
+      const nameA = a.name.toUpperCase();
+      const nameB = b.name.toUpperCase();
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      return 0;
+    },
+    getEndOfUrl: function (url) {
+      return Number(url.substr(url.lastIndexOf('/')+1));
+    }
+
   }
 }
 </script>
 
 <style lang='css' scoped>
-
+main {
+  width: 100vw;
+  background-image: url(https://wallpaper-mania.com/wp-content/uploads/2018/09/High_resolution_wallpaper_background_ID_77702110892.jpg);
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  color: #e1eef6;
+}
 </style>
