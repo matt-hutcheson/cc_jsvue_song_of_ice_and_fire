eec6e0e9bfcaea40b46f624a5435cc097b6e61ad
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2020-10-25 15:25:38 +0000

    finished character details html

--- src/components/characterDetails.vue
+++ src/components/characterDetails.vue
@@ -4,8 +4,30 @@
         <dl>
             <dt>Name:</dt>
             <dd>{{character.name}}</dd>
-            <dt>Aliases:</dt>
-            <dd v-for='(alias, index) in character.aliases' :key='index'>alias</dd>
+        </dl>
+        <dl v-if='character.aliases[0] != ""'>
+            <dt >Aliases:</dt>
+            <dd v-for='(alias, index) in character.aliases' :key='index'>{{alias}}</dd>
+        </dl>
+        <dl v-if='character.titles[0] != ""'>
+            <dt >Titles:</dt>
+            <dd v-for='(title,index) in character.titles' :key='index'>{{title}}</dd>
+        </dl>
+        <dl v-if='character.culture != ""'>
+            <dt >Culture:</dt>
+            <dd>{{character.culture}}</dd>
+        </dl>
+        <dl v-if='character.gender != ""'>
+            <dt >Gender:</dt>
+            <dd>{{character.gender}}</dd>
+        </dl>
+        <dl v-if='character.born != ""'>
+            <dt >Born:</dt>
+            <dd>{{character.born}}</dd>
+        </dl>
+        <dl v-if='character.playedBy[0] != ""'>
+            <dt >Played By:</dt>
+            <dd v-for='(playedBy,index) in character.playedBy' :key='index'>{{playedBy}}</dd>
         </dl>
   </article>
 </template>
